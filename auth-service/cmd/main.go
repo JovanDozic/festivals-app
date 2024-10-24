5736470a4ab84e0329749f22611d5a01f615d6a0
Jovan Dozic <jovandozic@gmail.com>
2024-10-24 20:06:00 +0200

    DIPL-44: add base files to all services

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"auth-serivce/internal/config"
+	"auth-serivce/internal/db"
+	"log"
+)
+
+func main() {
+
+	var config config.Config
+	config.Load()
+	if err := config.Validate(); err != nil {
+		log.Fatalln("error loading configuration:", err)
+	}
+	log.Println("config loaded successfully")
+
+	db, err := db.Init(config.DB)
+	if err != nil {
+		log.Fatalln("error initializing database:", err)
+	}
+	log.Println("database initialized successfully")
+
+	// todo: remove this line
+	log.Println(db)
+
+	// todo: router
+
+	// todo: start server
+}
---
+++ user-service/internal/handlers/handler.go
@@ -0,0 +1,3 @@
+package handlers
+
+// This will be test handler, we will add health check here for now
