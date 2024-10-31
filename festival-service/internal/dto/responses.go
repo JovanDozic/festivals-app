23b6406962d34a52b2faeeb0a642d3f12473bca8
Jovan Dozic <jovandozic@gmail.com>
2024-10-31 21:00:43 +0100

    Fresh start

--- festival-service/internal/dto/responses.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type HealthCheckResponse struct {
-	ServiceName string `json:"service_name"`
-	Status      string `json:"status"`
-	Environment string `json:"environment"`
-	API         string `json:"API"`
-	Secure      bool   `json:"secure"`
-}
--- festival-service/internal/handlers/errors.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-import "net/http"
-
-func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
-	w.Write([]byte("405 Method Not Allowed"))
-}
