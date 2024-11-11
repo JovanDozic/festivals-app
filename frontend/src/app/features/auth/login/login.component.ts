2c3b917d370787d40168bbec0066ef63e3392fe1
Jovan Dozic <jovandozic@gmail.com>
2024-11-11 18:57:57 +0100

    add login form

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../../core/auth.service';
+import { MatCardActions, MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
+  loginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  errorMessage: string | null = null;
+
+  onSubmit(): void {
+    if (this.loginForm) {
+      this.authService
+        .login(this.loginForm.value as { username: string; password: string })
+        .subscribe({
+          next: () => {
+            alert('Login successful!');
+          },
+          error: (err) => {
+            console.error('Login error:', err);
+            this.errorMessage = 'Invalid username or password';
+          },
+        });
+    }
+  }
 }
