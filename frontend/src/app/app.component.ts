d3da839a9c90e6a213d76d297817e0aef4f3599d
Jovan Dozic <jovandozic@gmail.com>
2024-11-20 20:34:36 +0100

    add create festival UI with adding and previewing images

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,3 +12,10 @@ import { LayoutComponent } from './layout/layout.component';
 export class AppComponent {
   title = 'Festivals';
 }
+
+function formatDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
--- frontend/src/app/features/organizer/create-festival/create-festival.component.ts
+++ frontend/src/app/features/organizer/create-festival/create-festival.component.ts
@@ -15,6 +15,16 @@ import {
   CreateFestivalRequest,
   Festival,
 } from '../../../models/festival/festival.model';
+import { forkJoin } from 'rxjs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  ConfirmationDialog,
+  ConfirmationDialogData,
+} from '../../../shared/confirmation-dialog/confirmation-dialog.component';
+import { routes } from '../../../app.routes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-festival',
@@ -30,6 +40,8 @@ import {
     MatButtonModule,
     MatCardModule,
     MatDatepickerModule,
+    MatGridListModule,
+    MatIconModule,
   ],
   providers: [provideNativeDateAdapter()],
 })
@@ -38,12 +50,19 @@ export class CreateFestivalComponent {
   private http = inject(HttpClient);
   private snackbar = inject(MatSnackBar);
   private festivalService = inject(FestivalService);
+  private dialog = inject(MatDialog);
+  router: Router = inject(Router);
 
   @ViewChild('stepper') private stepper: MatStepper | undefined;
 
   isLinear = true;
   festivalId: string | null = null;
-  selectedFile: File | null = null;
+  images: string[] = [
+    'https://prismic-assets-cdn.tomorrowland.com/ZqJkiB5LeNNTxfzO_1721400232662_dbb1c34f-229a-46b2-81f3-f498bccf476c.jpg_0_10322487413277135069.jpg',
+    'https://venga-store.com/cdn/shop/articles/Tomorrowland-festival-outfits.jpg?v=1721132564&width=1920',
+    'https://prismic-assets-cdn.tomorrowland.com/ZwqbE4F3NbkBXXNu_1728686140913_7ef44b3b-297c-4a1c-9d05-a7b04824fc23.jpg_0_12063235528760173649.jpg?crop=3000%2C1600%2C0%2C200&width=600&height=320',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpolaAYzCeS-n-9KirQxBhe4Co9illDu3ffIlISCpVOIkHkiAiYwYWNQVGZWhvkSZlzzs&usqp=CAU',
+  ];
 
   basicInfoFormGroup = this.fb.group({
     nameCtrl: ['', Validators.required],
@@ -62,7 +81,9 @@ export class CreateFestivalComponent {
     countryISO3Ctrl: ['SRB', Validators.required],
   });
 
-  imagesFormGroup = this.fb.group({});
+  imagesFormGroup = this.fb.group({
+    imageUrlCtrl: [''],
+  });
 
   createFestivalBasicInfo() {
     if (this.basicInfoFormGroup.valid) {
@@ -76,21 +97,23 @@ export class CreateFestivalComponent {
         name: this.basicInfoFormGroup.get('nameCtrl')?.value ?? '',
         description:
           this.basicInfoFormGroup.get('descriptionCtrl')?.value ?? '',
-        startDate: this.basicInfoFormGroup.get('startDateCtrl')?.value ?? '',
-        endDate: this.basicInfoFormGroup.get('endDateCtrl')?.value ?? '',
+        startDate: formatDate(
+          new Date(this.basicInfoFormGroup.get('startDateCtrl')?.value ?? '')
+        ),
+        endDate: formatDate(
+          new Date(this.basicInfoFormGroup.get('endDateCtrl')?.value ?? '')
+        ),
         capacity:
           Number(this.basicInfoFormGroup.get('capacityCtrl')?.value) ?? 0,
         address: {
-          street:
-            this.addressFormGroup.get('streetCtrl')?.value ?? 'Temp Street',
-          number: this.addressFormGroup.get('numberCtrl')?.value ?? '1',
+          street: this.addressFormGroup.get('streetCtrl')?.value ?? '',
+          number: this.addressFormGroup.get('numberCtrl')?.value ?? '',
           apartmentSuite:
             this.addressFormGroup.get('apartmentSuiteCtrl')?.value ?? '',
-          city: this.addressFormGroup.get('cityCtrl')?.value ?? 'Koralovo',
-          postalCode:
-            this.addressFormGroup.get('postalCodeCtrl')?.value ?? '99999',
+          city: this.addressFormGroup.get('cityCtrl')?.value ?? '',
+          postalCode: this.addressFormGroup.get('postalCodeCtrl')?.value ?? '',
           countryISO3:
-            this.addressFormGroup.get('countryISO3Ctrl')?.value ?? 'SRB',
+            this.addressFormGroup.get('countryISO3Ctrl')?.value ?? '',
         },
       };
 
@@ -113,53 +136,61 @@ export class CreateFestivalComponent {
     }
   }
 
-  onFileSelected(event: Event) {
-    const target = event.target as HTMLInputElement;
-    if (target.files && target.files.length > 0) {
-      this.selectedFile = target.files[0];
+  addImage() {
+    if (this.imagesFormGroup.valid) {
+      const imageUrl = this.imagesFormGroup.get('imageUrlCtrl')?.value ?? '';
+      this.images.push(imageUrl);
+      this.imagesFormGroup.reset();
     }
   }
 
-  onDragOver(event: DragEvent) {
-    event.preventDefault();
-    event.stopPropagation();
+  removeImage(url: string) {
+    const dialogRef = this.dialog.open(ConfirmationDialog, {
+      data: {
+        title: 'Remove Image',
+        message: `Are you sure?`,
+        confirmButtonText: 'Remove',
+        cancelButtonText: 'Cancel',
+      } as ConfirmationDialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result?.confirm) {
+        this.images = this.images.filter((image) => image !== url);
+      }
+    });
   }
 
-  onDrop(event: DragEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
-      this.selectedFile = event.dataTransfer.files[0];
+  uploadFestivalImages() {
+    if (this.images.length === 0) {
+      this.snackbar.open('Add at least one image');
     }
-  }
-
-  uploadFestivalImage() {
-    if (this.selectedFile && this.festivalId) {
-      const formData = new FormData();
-      formData.append('image', this.selectedFile);
-
-      this.http
-        .post(
+    if (this.festivalId && this.images.length > 0) {
+      const uploadObservables = this.images.map((imageUrl) =>
+        this.http.post(
           `http://localhost:4000/festival/${this.festivalId}/image`,
-          formData
+          { imageUrl }
         )
-        .subscribe({
-          next: () => {
-            this.snackbar.open('Image uploaded successfully!', 'Close', {
-              duration: 2000,
-            });
-            setTimeout(() => {
-              window.location.reload();
-            }, 1000);
-          },
-          error: (error) => {
-            console.error('Error uploading image:', error);
-            this.snackbar.open('Error uploading image', 'Close', {
-              duration: 2000,
-            });
-          },
-        });
+      );
+
+      forkJoin(uploadObservables).subscribe({
+        next: () => {
+          this.snackbar.open('Festival created successfully!');
+          this.stepper?.next();
+          this.router.navigate(['organizer/my-festivals']);
+        },
+        error: (error) => {
+          console.error('Error uploading images:', error);
+          this.snackbar.open('Error uploading images');
+        },
+      });
     }
   }
+}
+
+function formatDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 }
