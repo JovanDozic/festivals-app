a7dd52b3ebec659b429b1b043b8f458771fd5a5c
Jovan Dozic <jovandozic@gmail.com>
2024-12-01 13:50:03 +0100

    add page transitions

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,5 @@ import { LayoutComponent } from './layout/layout.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Festivals';
-}
-
-function formatDate(date: Date): string {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  title = 'Festivals App';
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
@@ -12,7 +12,7 @@ import { AuthInterceptor } from './core/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
