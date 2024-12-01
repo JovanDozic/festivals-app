06320408bb3662d9624fb4e0790aa99142fa438f
Jovan Dozic <jovandozic@gmail.com>
2024-12-01 16:47:58 +0100

    add basic code to all package addon components

--- frontend/src/app/features/organizer/package-addons/camp-package-addons/camp-package-addons.component.ts
+++ frontend/src/app/features/organizer/package-addons/camp-package-addons/camp-package-addons.component.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { Festival } from '../../../../models/festival/festival.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FestivalService } from '../../../../services/festival/festival.service';
+import { SnackbarService } from '../../../../shared/snackbar/snackbar.service';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatMenuModule } from '@angular/material/menu';
+import { ItemService } from '../../../../services/festival/item.service';
 @Component({
   selector: 'app-camp-package-addons',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatCardModule,
+    MatChipsModule,
+    MatMenuModule,
+  ],
   templateUrl: './camp-package-addons.component.html',
-  styleUrl: './camp-package-addons.component.scss'
+  styleUrls: [
+    './camp-package-addons.component.scss',
+    '../../../../app.component.scss',
+  ],
 })
 export class CampPackageAddonsComponent {
+  isLoading: boolean = true;
+  festival: Festival | null = null;
+  generalCount: number = 0;
+  transportCount: number = 0;
+  campCount: number = 0;
 
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private festivalService = inject(FestivalService);
+  private snackbarService = inject(SnackbarService);
+  private itemService = inject(ItemService);
+  private dialog = inject(MatDialog);
+
+  ngOnInit() {
+    this.loadFestival();
+    this.loadCounts();
+  }
+
+  loadCounts() {
+    throw new Error('Method not implemented.');
+  }
+
+  goBack() {
+    this.router.navigate([
+      `organizer/my-festivals/${this.festival?.id}/package-addons`,
+    ]);
+  }
+
+  loadFestival() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.festivalService.getFestival(Number(id)).subscribe({
+        next: (festival) => {
+          this.festival = festival;
+          this.isLoading = false;
+          console.log(
+            `Festival ID: <${this.festival?.id}> - ${this.festival?.name}`
+          );
+        },
+        error: (error) => {
+          console.log('Error fetching festival information: ', error);
+          this.snackbarService.show('Error getting festival');
+          this.festival = null;
+          this.isLoading = true;
+        },
+      });
+    }
+  }
 }
