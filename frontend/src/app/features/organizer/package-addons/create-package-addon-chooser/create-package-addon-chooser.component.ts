46eaa1585d3433d5462a9fe4ce1f56b5f022f23a
Jovan Dozic <jovandozic@gmail.com>
2024-12-02 20:32:35 +0100

    fix ui

--- frontend/src/app/features/organizer/package-addons/create-package-addon-chooser/create-package-addon-chooser.component.ts
+++ frontend/src/app/features/organizer/package-addons/create-package-addon-chooser/create-package-addon-chooser.component.ts
@@ -1,23 +1,6 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import {
-  MAT_DIALOG_DATA,
-  MatDialogModule,
-  MatDialogRef,
-} from '@angular/material/dialog';
-import {
-  FormArray,
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  ValidationErrors,
-  Validators,
-} from '@angular/forms';
-import {
-  CreateItemPriceRequest,
-  CreateItemRequest,
-  VariablePrice,
-} from '../../../../models/festival/festival.model';
+import { Component, inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -26,13 +9,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { SnackbarService } from '../../../../shared/snackbar/snackbar.service';
-import { MatStepper, MatStepperModule } from '@angular/material/stepper';
-import { ItemService } from '../../../../services/festival/item.service';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { forkJoin } from 'rxjs';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 
@@ -68,9 +47,8 @@ interface Category {
     '../../../../app.component.scss',
   ],
 })
-export class CreatePackageAddonChooserComponent implements OnInit {
+export class CreatePackageAddonChooserComponent {
   private dialogRef = inject(MatDialogRef<CreatePackageAddonChooserComponent>);
-  private data: { festivalId: number } = inject(MAT_DIALOG_DATA);
 
   categories: Category[] = [
     {
@@ -90,9 +68,7 @@ export class CreatePackageAddonChooserComponent implements OnInit {
     },
   ];
 
-  selectedCategory: any | null = this.categories[2].value;
-
-  ngOnInit(): void {}
+  selectedCategory: any | null = null;
 
   closeDialog() {
     this.dialogRef.close(false);
