52e95a71cc196ae5867e38fa0123fe712da5e50d
Jovan Dozic <jovandozic@gmail.com>
2024-12-12 20:42:14 +0100

    add files

---
+++ frontend/src/app/features/admin/all-accounts/all-accounts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-accounts',
+  imports: [],
+  templateUrl: './all-accounts.component.html',
+  styleUrl: './all-accounts.component.scss'
+})
+export class AllAccountsComponent {
+
+}
